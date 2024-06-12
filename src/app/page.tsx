043dd34502d682e428996ac0da4efb30f5211e03
HoodScp <godpd440@naver.com>
2024-06-12 10:44:48 +0900

    image plus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,21 @@ export default function Home() {
       </div>
       <div className="font-mono bg-blue-600 text-blue-300 text-center m-4">
         <p>1 + 2 = {result}</p>
+        <p>
+          node js 안에 슬라이드쇼와 같은 기능을 어떻게 구현할 지 모르겠어요.
+        </p>
+        <p>
+          나중에 방학때 시간이 된다면 node js 를 사용하는 법에 대해서 더
+          면밀하게 공부해보고 싶어요.
+        </p>
+      </div>
+      <div className="font-mono bg-blue-600 text-blue-300 text-center m-4 flex justify-center">
+        <Image
+          src="/image_47.png"
+          alt="guitar.jpg"
+          width="500"
+          height="300"
+        ></Image>
       </div>
       <Footer />
     </main>
